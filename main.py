38080cfbf0a822713ed2ea116767f7c4e0cbaf49
Exilenz <vaclavek.ales@gmail.com>
2024-12-20 18:56:23 +0100

    new

---
+++ main.py
@@ -0,0 +1,60 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import sys
+import pygame
+from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import Shot
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
+
